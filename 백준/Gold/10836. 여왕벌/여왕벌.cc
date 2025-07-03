6ea59d15fcaab922ce9ab2aa71ce15514c18d3a4
hyunseok99 <78286660+hyunseok99@users.noreply.github.com>
2025-07-03 17:22:47 +0900

    [Gold III] Title: 여왕벌, Time: 116 ms, Memory: 5880 KB, Score: 83 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.cc
+++ 백준/Gold/10836. 여왕벌/여왕벌.cc
@@ -1,59 +1,13 @@
-
-
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <utility>
 using namespace std;
 
+
 int n, m;
 
-// 방향 L, D, U
-int dx[3] = { 0, -1, -1 };
-int dy[3] = { -1, -1, 0 };
 
-void doUp(vector<vector<pair<int, int>>>& node) {
-	// x축 연산 이후 y축 연산 
-	for (int i = 1; i < m; i++) {
-
-		// x축 
-		for (int j = i; j < m; j++) {
-
-			int startX = i;
-			int startY = j;
-			// 커지는 정도 갱신
-			int add = 0; // 증분
-			for (int dir = 0; dir < 3; dir++) {
-				int nx = startX + dx[dir];
-				int ny = startY + dy[dir];
-				add = max(add, node[nx][ny].second);
-				if (dir == 2) {
-					node[startX][startY].second = add;
-				}
-			}
-			// 크기 갱신
-			node[startX][startY].first += node[startX][startY].second;
-		}
-
-		// y축
-		for (int j = i + 1; j < m; j++) {
-			int startX = j;
-			int startY = i;
-			int add = 0;
-			for (int dir = 0; dir < 3; dir++) {
-				int nx = startX + dx[dir];
-				int ny = startY + dy[dir];
-				add = max(add, node[nx][ny].second);
-				if (dir == 2) {
-					node[startX][startY].second = add;
-				}
-			}
-			// 크기 갱신 
-			node[startX][startY].first += node[startX][startY].second;
-		}
-	}
-	return;
-}
 
 int main() {
 	cin >> m >> n;
@@ -73,18 +27,24 @@ int main() {
 		}
 
 		// init (2m-1 개)
+		// x = 0 일떄의 y값의 증분의 합으로 동일 
 		for (int j = 0; j < input.size(); j++) {
 			if (j < m) {
 				node[m - (j + 1)][0].first += input[j];
-				node[m - (j + 1)][0].second = input[j];
+				node[m - (j + 1)][0].second += input[j];
 			}
 			else {
 				node[0][j - m + 1].first += input[j];
-				node[0][j - m + 1].second = input[j];
+				node[0][j - m + 1].second += input[j];
 			}
 		}
+	}
 
-		doUp(node);
+
+	for (int i = 1; i < m; i++) {
+		for (int j = 1; j < m; j++) {
+			node[i][j].first += node[0][j].second;
+		}
 	}
 
 	for (int i = 0; i < m; i++) {
