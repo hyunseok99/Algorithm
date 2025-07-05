7c405b691b8d6ae1271619517f4c989b4d325a1f
hyunseok99 <78286660+hyunseok99@users.noreply.github.com>
2025-07-05 17:14:17 +0900

    [Gold III] Title: 여왕벌, Time: 828 ms, Memory: 5876 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.cc
+++ 백준/Gold/10836. 여왕벌/여왕벌.cc
@@ -7,45 +7,50 @@ using namespace std;
 
 int n, m;
 
-
-
 int main() {
 	cin >> m >> n;
 	// pair.first: 애벌래 크기, second: 커지는 정도
+	// 핵심은 별레가 커지는 정도는 해당 좌표의 x축이 0인 값의 증분이 답임 
 	vector<vector<pair<int, int>>> node(m, vector<pair<int, int>>(m, { 1,0 }));
 
-	for (int i = 0; i < n; i++) {
+	// idxLine: 커지는 정도 증가량의 변화가 생기는 idx 
+	// ex) 2 3 2 -> 0 1| 2 3 4 | 5 6  -> 경계는 2, 5   ,  0 6 1 ->  | 0 1 2 3 4 5  | 6  -> 경계는 0, 6 , 0 0 7 ->  | | 123.. -> 0 0
+	// idx: 0,1,2 ... -> [m-1][0], [m-2][0]... [0][0], ... [0][m-1] 
+	vector<int> idxLine(2 * m - 1 );
 
-		vector<int> input;
-		// 0,1,2 갯수 입력
+	for (int i = 0; i < n; i++) {
+		// 0,1,2 갯수 입력 -> 필요한건 0->1 경계, 1->2 경계
+		int idx = 0;
 		for (int j = 0; j < 3; j++) {
 			int t;
 			cin >> t;
-			for (int k = 0; k < t; k++) {
-				input.push_back(j);
-			}
-		}
-
-		// init (2m-1 개)
-		// x = 0 일떄의 y값의 증분의 합으로 동일 
-		for (int j = 0; j < input.size(); j++) {
-			if (j < m) {
-				node[m - (j + 1)][0].first += input[j];
-				node[m - (j + 1)][0].second += input[j];
-			}
-			else {
-				node[0][j - m + 1].first += input[j];
-				node[0][j - m + 1].second += input[j];
+			if (j < 2) {
+				idx += t;
+				if (idx >= (2 * m - 1)) continue;
+				idxLine[idx]++;
 			}
 		}
 	}
 
-
-	for (int i = 1; i < m; i++) {
-		for (int j = 1; j < m; j++) {
-			node[i][j].first += node[0][j].second;
+	int sizeRatio = 0;
+	for (int i = 0; i < idxLine.size(); i++) {
+		if (idxLine[i] != 0) {
+			sizeRatio += idxLine[i];
+		}
+		if (m - i - 1 >= 0) {
+			node[m - 1 - i][0].first += sizeRatio;
+			node[m - 1 - i][0].second += sizeRatio;
+		}
+		else {
+			node[0][i - m + 1].first += sizeRatio;
+			node[0][i - m + 1].second += sizeRatio;
+			// 나머지 갱신
+			for (int j = 1; j < m; j++) {
+				node[j][i - m + 1].first += sizeRatio;
+			}
 		}
 	}
+
 
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < m; j++) {
