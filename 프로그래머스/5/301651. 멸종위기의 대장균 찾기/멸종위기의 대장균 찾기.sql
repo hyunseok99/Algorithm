9d504f7acd13abe36a8109605e909bb28b27f988
hyunseok99 <78286660+hyunseok99@users.noreply.github.com>
2025-09-22 15:55:33 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,31 +1,63 @@
--- 코드를 작성해주세요
+# -- 코드를 작성해주세요
+# WITH RECURSIVE generation AS(
+#     # init
+#     SELECT a.ID, a.PARENT_ID, 1 AS LEV, (
+#         SELECT COUNT(e.PARENT_ID)
+#         FROM ECOLI_DATA e
+#         WHERE a.ID = e.PARENT_ID 
+#         GROUP BY e.PARENT_ID
+#     ) as CHILD
+#     FROM ECOLI_DATA a
+#     WHERE a.PARENT_ID IS NULL
+
+#     UNION ALL
+    
+#     # recursion
+#     SELECT e.ID, e.PARENT_ID, g.LEV+1, (
+#         SELECT COUNT(x.PARENT_ID)
+#         FROM ECOLI_DATA x
+#         WHERE e.ID = x.PARENT_ID 
+#         GROUP BY x.PARENT_ID
+#     ) as CHILD
+#     FROM ECOLI_DATA e, generation g
+#     WHERE g.ID = e.PARENT_ID
+# )
+
+
+# SELECT COUNT(g.ID) as COUNT, g.LEV as GENERATION
+# FROM generation g
+# WHERE g.CHILD IS NULL
+# GROUP By g.LEV
+# ORDER BY g.LEV asc;
+
+
+
+
+
+# 다른 방식: 기존의 세대까지만 구한 후 -> 일반 테이블과 조인해서 갯수 
 WITH RECURSIVE generation AS(
     # init
-    SELECT a.ID, a.PARENT_ID, 1 AS LEV, (
-        SELECT COUNT(e.PARENT_ID)
-        FROM ECOLI_DATA e
-        WHERE a.ID = e.PARENT_ID 
-        GROUP BY e.PARENT_ID
-    ) as CHILD
-    FROM ECOLI_DATA a
-    WHERE a.PARENT_ID IS NULL
-
+    SELECT ID, 1 as LEV
+    FROM ECOLI_DATA
+    WHERE PARENT_ID IS NULL
+    
     UNION ALL
     
-    # recursion
-    SELECT e.ID, e.PARENT_ID, g.LEV+1, (
-        SELECT COUNT(x.PARENT_ID)
-        FROM ECOLI_DATA x
-        WHERE e.ID = x.PARENT_ID 
-        GROUP BY x.PARENT_ID
-    ) as CHILD
-    FROM ECOLI_DATA e, generation g
-    WHERE g.ID = e.PARENT_ID
+    SELECT e.ID, g.LEV+1
+    FROM generation g, ECOLI_DATA e
+    WHERE e.PARENT_ID = g.ID
 )
 
-
-SELECT COUNT(g.ID) as COUNT, g.LEV as GENERATION
+SELECT COUNT(g.ID) as COUNT,g.LEV as generation
 FROM generation g
-WHERE g.CHILD IS NULL
-GROUP By g.LEV
+LEFT JOIN ECOLI_DATA e ON g.ID = e.PARENT_ID
+WHERE e.ID IS NULL
+GROUP BY g.LEV
 ORDER BY g.LEV asc;
+
+
+
+
+
+
+
